6baa8eb954d3e6a11a9be72d7fca5368c65cc941
zoobsbaqai <zohairbaqai@hotmail.com>
2025-06-10 14:21:23 -0400

    fifth

--- ExerciseLogPane.java
+++ ExerciseLogPane.java
@@ -1,3 +1,13 @@
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.*;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExerciseLogPane {
-    
+    public static void show(JFrame previousFrame){
+        
+    }
 }
--- FoodLogPane.java
+++ FoodLogPane.java
@@ -1,3 +1,13 @@
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.*;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class FoodLogPane {
-    
+    public static void show(JFrame previousFrame){}
+
 }
